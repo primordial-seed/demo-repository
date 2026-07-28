3a36834f681b5773ba004b74d413ba695ba3ca8a
Han Nguyen <han.t.nguyen@proton.me>
2026-07-28 06:25:54 -0400

    remove export

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const DUMMY_CONSTANT =
-  "This is a dummy constant for demonstration purposes.";
+const DUMMY_CONSTANT = "This is a dummy constant for demonstration purposes.";
+console.log(DUMMY_CONSTANT);
